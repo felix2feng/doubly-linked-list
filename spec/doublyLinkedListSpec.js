3ef67d31e20be7f6110b78f3a190357ee11fa7f9
Hack Reactor Students <accounts@hackreactor.com>
2016-04-23 11:26:22 -0700

    added some more tests

--- spec/doublyLinkedListSpec.js
+++ spec/doublyLinkedListSpec.js
@@ -10,17 +10,51 @@ describe('doublyLinkedList', function() {
     expect(doubleList).to.have.property('tail');
   });
 
-  it('should have methods named "insert", "removeHead", and "contains"', function() {
-    expect(doubleList.insert).to.be.a('function');
-    expect(doubleList.removeHead).to.be.a('function');
+  it('should have methods named "addToHead", "removeTail", and "contains"', function() {
+    expect(doubleList.addToHead).to.be.a('function');
+    expect(doubleList.removeTail).to.be.a('function');
     expect(doubleList.contains).to.be.a('function');
   });
 
-  it('should re-point surrounding nodes when inserting a new one', function() {
-    doubleList.insert(0);
-    doubleList.insert(1);
-    doubleList.insert(2);
-    expect(doubleList[1].next.value.to.equal(2));
-    expect(doubleList[1].previous.value.to.equal(0));
+  it('should re-point surrounding nodes to new one when removing', function() {
+    doubleList.addToHead(0);
+    doubleList.addToHead(1);
+    doubleList.addToHead(2);
+    expect(doubleList[1].next.value).to.equal(2);
+    expect(doubleList[1].previous.value).to.equal(0);
   });
+
+  it('should return value of the former tail when removeTail is called', function() {
+    doubleList.addToHead('Michael-Daly');
+    doubleList.addToHead('Felix Feng');
+    expect(doubleList.removeTail()).to.equal('Felix Feng');
+  });
+
+  it('should contain a value that was added', function() {
+    doubleList.addToHead('Hack Reactor Rocks!');
+    doubleList.addToHead(8);
+    doubleList.addToHead('But I don\'t like waking up at 8:00A.M.');
+    expect(doubleList.contains('Hack Reactor Rocks!')).to.equal(true);
+    expect(doubleList.contains(8)).to.equal(true);
+    expect(doubleList.contains(7)).to.equal(false);
+  });
+
+  it('should not contain a value that was removed', function() {
+    doubleList.addToHead(5);
+    doubleList.addToHead(4);
+    doubleList.addToHead(3);
+    doubleList.removeTail();
+    expect(doubleList.contains(5)).to.equal(false);
+  })
+
 });
+
+
+
+
+// Has the following properties
+// nodes need next and previous properties, default to null
+// 
+// .addToHead() <- takes a value and adds to front
+// .removeTail <- Remove last node and return value
+// each node will have new .previous property 
